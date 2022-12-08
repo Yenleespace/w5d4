4b94926ee30f4fd7b6b2a2b152e4cc4feededd73
Yen Lee <yenny0104@gmail.com>
2022-12-08 16:46:04 -0500

    user table

--- w5d4/practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
+++ w5d4/practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
@@ -9,6 +9,14 @@
 #
 class User < ApplicationRecord
 
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :student_id,
+        class_name: :Enrollment
     
+    has_many :courses,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Course
    
 end
